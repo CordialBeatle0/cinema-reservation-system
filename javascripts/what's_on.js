ab1b27b36aebb388d1a05b064beedd038465eb62
SheroukHesham <sheroukhesham2000@hotmail.com>
2024-04-08 21:32:39 +0200

    added sherouk stuff init

---
+++ javascripts/what's_on.js
@@ -0,0 +1,16 @@
+const myDiv = document.getElementById("myDiv");
+myDiv.addEventListener("mouseover", showTooltip);
+myDiv.addEventListener("mouseout", hideTooltip);
+
+window.onload = function () {
+  hideTooltip();
+};
+
+function hideTooltip() {
+  const tooltip = document.querySelector(".tooltip");
+  tooltip.style.display = "none";
+}
+function showTooltip() {
+  const tooltip = document.querySelector(".tooltip");
+  tooltip.style.display = "block";
+}
