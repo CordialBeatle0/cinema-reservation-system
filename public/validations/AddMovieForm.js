a5ce7fb865a30344f0770de2d8a327d14b4e07fe
assarhegazy <125737782+Assarhegazy@users.noreply.github.com>
2024-04-11 01:28:18 +0300

    Addmovie from done with all validations

--- public/validations/AddMovieForm.js
+++ public/validations/AddMovieForm.js
@@ -1,6 +1,12 @@
 // All  input values in form
 const form = document.querySelector("#submit-form");
-
+const posterInput = document.getElementById("poster");
+const trailerInput = document.getElementById("trailer");
+const movienameInput = document.getElementById("moviename");
+const releasedateInput = document.getElementById("releasedate");
+const genreInput = document.getElementById("genre");
+const runningtimeInput = document.getElementById("runningtime");
+const descriptionInput = document.getElementById("description");
 
 // Function to display error messages
 const showError = (field, errorText) => {
@@ -11,40 +17,18 @@ const showError = (field, errorText) => {
   field.closest(".form-group").appendChild(errorElement);
 };
 
-const confirmBtn = document.querySelector("#confirm-btn");
-confirmBtn.addEventListener("click", () => {
-  validatePrevious();
-});
-
-
-function validatePrevious() {
-  
- 
-
-  
-  // Clearing previous error messages
-  document
-    .querySelectorAll(".form-group .error")
-    .forEach((field) => field.classList.remove("error"));
-  document
-    .querySelectorAll(".error-text")
-    .forEach((errorText) => errorText.remove());
-
-    // Performing validation checks
-    
- 
-  const errorInputs = document.querySelectorAll(".form-group .error");
-  if (errorInputs.length === 0) {
-    $(".confirm_details-btn").click(function () {
-      $(".payment-form").slideDown();
-    });
-  }
-}
-
 // Function to handle form submission // checks for errors
 const handleFormData = (e) => {
   e.preventDefault();
 
+  // trimmed values from inputs
+  const poster = posterInput.value.trim();
+  const trailer = trailerInput.value.trim();
+  const moviename = movienameInput.value.trim();
+  const releasedate = releasedateInput.value.trim();
+  const genre = genreInput.value.trim();
+  const runningtime = runningtimeInput.value.trim();
+  const description = descriptionInput.value.trim();
 
   // Clearing previous error messages
   document
@@ -54,13 +38,38 @@ const handleFormData = (e) => {
     .querySelectorAll(".error-text")
     .forEach((errorText) => errorText.remove());
 
-  
+  //validation checks
+  if (poster === "") {
+    showError(posterInput, "Poster Required");
+  }
+  if (trailer === "") {
+    showError(trailerInput, "Trailer Required");
+  }
+  if (moviename === "") {
+    showError(movienameInput, "Movie Name Required");
+  }
+
+  if (releasedate === "") {
+    showError(releasedateInput, "Release Date Required");
+  }
+  if (genre === "") {
+    showError(genreInput, "Genre Required");
+  }
+  if (runningtime === "") {
+    showError(runningtimeInput, "Running Time Required");
+  }
+
+  if (description === "") {
+    showError(descriptionInput, "Description Required");
+  }
+
   // Checking for any remaining errors before form submission
   const errorInputs = document.querySelectorAll(".form-group .error");
   if (errorInputs.length > 0) return;
 
   // Submitting the form
   form.submit();
+  alert("Movie added successfully!");
 };
 
 // Handling form submission event
