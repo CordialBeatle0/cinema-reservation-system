0ee1c916897d4af58a776a17815b6d2f89f5569d
assarhegazy <125737782+Assarhegazy@users.noreply.github.com>
2024-04-11 17:40:59 +0300

    Assar redoing the admin dash stuffies

--- public/js/AdminMovieDashboard.js
+++ public/js/AdminMovieDashboard.js
@@ -6,14 +6,14 @@ $(document).ready(function () {
     function () {
       $(this).css("font-size", "20px");
       $(this).css("background", "#146fef");
-      $(this).css("text-decoration", "underline");
+
       $(this).css("box-shadow", "0px 0px 10px 2px rgba(0,0,0,.8)");
     },
     function () {
       $(this).css("font-size", "15px");
       $(this).css("background", " #33b0de");
-        $(this).css("text-decoration", "none");
-        $(this).css("box-shadow", "inset 0 0 50px #146fef");
+      $(this).css("text-decoration", "none");
+      $(this).css("box-shadow", "inset 0 0 50px #146fef");
     }
   );
   // add movie button
@@ -21,7 +21,7 @@ $(document).ready(function () {
     function () {
       $(this).css("font-size", "20px");
       $(this).css("background", "#146fef");
-      $(this).css("text-decoration", "underline");
+
       $(this).css("box-shadow", "0px 0px 10px 2px rgba(0,0,0,.8)");
     },
     function () {
