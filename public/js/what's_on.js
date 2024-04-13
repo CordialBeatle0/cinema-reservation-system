4d42d36a5f93f2f253b598047314165020b645a2
SheroukHesham <sheroukhesham2000@hotmail.com>
2024-04-13 03:56:45 +0200

    whats on show button + coming soon page + front#1

--- public/js/what's_on.js
+++ public/js/what's_on.js
@@ -1,3 +1,4 @@
+// javascript for showing movie tooltip on card hover (on up from the poppy hill card)
 const myDiv = document.getElementById("activate");
 myDiv.addEventListener("mouseover", showTooltip);
 myDiv.addEventListener("mouseout", hideTooltip);
@@ -14,3 +15,26 @@ function showTooltip() {
   const tooltip = document.querySelector("#tooltip");
   tooltip.style.display = "block";
 }
+
+// jQuery for show more and show less button
+if ($(".card").length > 8) {
+  $(".card:gt(7)").hide();
+  $(".show-more").show();
+}
+
+$(".show-more").on("click", function () {
+  //toggle elements with class .ty-compact-list that their index is bigger than 2
+  $(".card:gt(7)").toggle();
+  //change text of show more element just for demonstration purposes to this demo
+  // $(this).text() === "Show more"
+  //   ? $(this).text("Show less")
+  //   : $(this).text("Show more");
+
+  if ($(this).text() === "Show more") {
+    $(this).text("Show less");
+    $(".bar").css("width", "100%");
+  } else {
+    $(this).text("Show more");
+    $(".bar").css("width", "50%");
+  }
+});
